fcf9ff3dc2c9ca6728fc02d3c995acb1c4c7263a
Solmi <0ursenh@gmail.com>
2023-05-18 07:56:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
  * main - Entry point
- * Return : Always 0 (Success)
+ *
+ * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	Return(0);
+	return (0);
 }
